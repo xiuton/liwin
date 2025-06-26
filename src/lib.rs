c75c8b27216cee597fdc1b8951c6a62206c5795d
gantoho <i@ganto.me>
2025-06-26 21:51:01 +0800

    feat: config.rs中的load函数的使用

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use dioxus_web::launch::launch;
+use crate::config::AppConfig;
 
 mod app;
 mod components;
@@ -9,5 +10,10 @@ mod config;
 #[wasm_bindgen::prelude::wasm_bindgen]
 pub fn main() {
     wasm_logger::init(wasm_logger::Config::default());
+    #[cfg(target_arch = "wasm32")]
+    let config = AppConfig::get_default();
+    #[cfg(not(target_arch = "wasm32"))]
+    let config = AppConfig::load().expect("配置加载失败");
+    println!("当前配置: {:?}", config);
     launch(app::app, Vec::new(), Vec::new());
 }
